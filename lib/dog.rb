7f5707401de2a10ab7a90ad3c500013e2d3bbf0a
Cailie Skelton <cskelton707@gmail.com>
2020-04-25 01:42:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,16 +2,13 @@ class Dog
   def bark
     puts "Woof!"
   end
-end
- 
-fido = Dog.new
-fido.bark
 
-class Dog
   def sit 
     puts "The Dog is Sitting"
   end
 end
 
 fido = Dog.new
+fido.bark
+
 fido.sit
